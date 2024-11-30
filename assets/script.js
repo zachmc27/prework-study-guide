e6c01aed4241e9c95db7933b0ae4e5afa22a6a1b
zachmc27 <dzdudes1213@gmail.com>
2024-11-30 17:48:13 -0600

    added functions to code

--- assets/script.js
+++ assets/script.js
@@ -1,22 +1,31 @@
-const topics = ['HTML', 'CSS', 'Git', 'Javascript']
+const topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+const randomTopic = topics[Math.floor(Math.random() * topics.length)];
 
+function listTopics() {
 for (i = 0; i < topics.length; i++) {
   console.log(topics[i])
-}
+};
+};
 
-/*
-if (topic === 'HTML') {
+function selectTopic() {
+if (randomTopic === 'HTML') {
  console.log("Let's study HTML!");
-} else if (topic === 'CSS') {
+} else if (randomTopic === 'CSS') {
  console.log("Let's study CSS!");
-} else if (topic === 'Git') {
+} else if (randomTopic === 'Git') {
  console.log("Let's study Git!");
-} else if (topic === 'JavaScript') {
+} else if (randomTopic === 'JavaScript') {
  console.log("Let's study JavaScript!");
 } else {
  console.log('Please try again!');
-}
-*/
+};
+};
+
+console.log('Here are the topics we learned through Prework:')
+listTopics()
+console.log('Which topic should we study first?')
+selectTopic()
+
 
 
 
